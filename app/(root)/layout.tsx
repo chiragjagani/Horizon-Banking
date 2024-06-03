7232f5188334ad1fdcb092fbef71847f242157f0
chirag <jaganichirag912@outlook.com>
2024-06-03 20:58:41 +0530

    implement Sidebar and MobileNav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,63 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+// import { getLoggedInUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // const loggedIn = "Chirag";
+
+  // if (!loggedIn) redirect("/sign-in");
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar
+        user={{
+          $id: "",
+          email: "",
+          userId: "",
+          dwollaCustomerUrl: "",
+          dwollaCustomerId: "",
+          firstName: "",
+          lastName: "",
+          address1: "",
+          city: "",
+          state: "",
+          postalCode: "",
+          dateOfBirth: "",
+          ssn: "",
+        }}
+      />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav
+              user={{
+                $id: "",
+                email: "",
+                userId: "",
+                dwollaCustomerUrl: "",
+                dwollaCustomerId: "",
+                firstName: "",
+                lastName: "",
+                address1: "",
+                city: "",
+                state: "",
+                postalCode: "",
+                dateOfBirth: "",
+                ssn: "",
+              }}
+            />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,43 @@
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
+import HearderBox from "@/components/HearderBox";
 
 const Home = () => {
-  return <div>Dashbord</div>;
+  const loggedIn = { firstName: "Chirag" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HearderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access to and mange your account and transaction efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.5}
+          />
+        </header>
+
+        {/* <RecentTransactions
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        /> */}
+      </div>
+
+      {/* <RightSidebar
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      /> */}
+    </section>
+  );
 };
 
 export default Home;
